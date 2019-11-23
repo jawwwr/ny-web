7ac14b6116f733c8118160180fb8ef2e399f8fc5
jawwwr <jarielb96@gmail.com>
2019-11-23 08:24:43 +0800

    update jenkins pipeline

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api_url = process.env.REACT_APP_NY_API_HOST
+const api_url = 'http://3.0.178.112'
 
 console.log(process.env)
 
@@ -9,9 +9,7 @@ const API = (method:any, url:any, body?:any) => {
     method,
     url: `${api_url}/${url}`,
     data: body,
-    headers: {
-      Authorization: `Bearer ${process.env.REACT_APP_TRANSFERWISE_TOKEN_SB}`,
-    },
+    headers: {},
   });
   return response
 }
